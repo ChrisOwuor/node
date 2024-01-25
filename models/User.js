14c97c51787de70ebaa5028435149a8e64f90e92
chrispine owuor <owuorchrispine77@gmail.com>
2024-01-25 23:54:48 +0300

    finished sign up

--- models/User.js
+++ models/User.js
@@ -17,7 +17,7 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt();
-  this.password = await bcrypt.hash(this.password, salt);
+  this.password =await bcrypt.hash(this.password, salt);
   next();
 });
 const user = mongoose.model("user", userSchema);
--- routes/Authroutes.js
+++ routes/Authroutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const Authcontrollers = require("../controllers/Authcontrollers");
 const appRouter = express.Router();
 
-appRouter.post("/signup", Authcontrollers.signup);
-appRouter.post("/login", Authcontrollers.login);
+appRouter.post("/signup", Authcontrollers.signup_post);
+appRouter.get("/signup", Authcontrollers.signup_get);
+appRouter.get("/login", Authcontrollers.login_get);
+appRouter.post("/login", Authcontrollers.login_post);
 module.exports = appRouter;
